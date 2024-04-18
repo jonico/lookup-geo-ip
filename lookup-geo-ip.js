c8910afa2b08c4a34f10e4d2da9b60de5dda2355
Johannes Nicolai <johannes.nicolai@postman.com>
2024-04-18 15:00:09 +0200

    dynamic port

--- lookup-geo-ip.js
+++ lookup-geo-ip.js
@@ -57,6 +57,6 @@ const server = http.createServer(async (req, res) => {
   res.end(output);
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
